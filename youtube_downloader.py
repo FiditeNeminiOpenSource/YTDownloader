3dd303ef4ec6b95794bb7455dc36fa2fc3541350
Fidite Nemini <128890551+FiditeNemini@users.noreply.github.com>
2023-05-15 11:40:11 +1200

    Update youtube_downloader.py
    Changed some argparse handling options.

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -42,10 +42,34 @@ def download_playlist(playlist_id):
 
 
 if __name__ == "__main__":
+    # Define the help message
+    help_message = '''
+    This is a Python script that can be used to download YouTube videos or playlists. The script uses the `yt_dlp` library, which is a fork of `youtube-dl`. It can handle different types of YouTube URLs and allows the user to choose between downloading a video or a playlist.
+
+    Usage:
+        python youtube_downloader.py <url> -d=<type>
+
+    Arguments:
+        <url>                   The URL of the YouTube video or playlist.
+    
+    Options:
+        -d=<type>, --download=<type>
+                                Specify whether to download a video or playlist. Default is 'video'.
+        -h, --help              Show this help message and exit.
+
+    Examples:
+        To download a video:
+            python youtube_downloader.py "https://www.youtube.com/watch?v=5PS2p1AZzFY" -d=video
+
+        To download a playlist:
+            python youtube_downloader.py "https://www.youtube.com/playlist?list=PLXmi76euGSyyq1nw21U1M4tTsM0Zysayk" -d=playlist
+    '''
+
     # Parse the URL and the download type from the command line arguments
-    parser = argparse.ArgumentParser(description="Download a YouTube video or playlist.")
+    parser = argparse.ArgumentParser(description="Download a YouTube video or playlist.", add_help=False, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("url", help="The URL of the YouTube video or playlist.")
-    parser.add_argument("--download", help="Specify whether to download a video or playlist.", choices=["video", "playlist"], required=True)
+    parser.add_argument("-d", "--download", metavar="type", help="Specify whether to download a video or playlist. Default is 'video'.", choices=["video", "playlist"], default="video")
+    parser.add_argument("-h", "--help", action="help", default=argparse.SUPPRESS, help="Show this help message and exit.")
     args = parser.parse_args()
 
     # Parse the video or playlist ID from the URL
@@ -69,4 +93,3 @@ if __name__ == "__main__":
     else:
         print("Error: Invalid download type. The specified download type must be either 'video' or 'playlist'.")
         exit()
-
