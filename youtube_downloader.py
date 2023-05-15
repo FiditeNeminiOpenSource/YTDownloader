887a7985befb0ba6f64e46f2f1de2bd718c53f48
FiditeNemini <fiditenemini2023@outlook.com>
2023-05-15 13:00:06 +1200

    Changed instructions.

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -44,34 +44,27 @@ def download_playlist(playlist_id):
 if __name__ == "__main__":
     # Define the help message
     help_message = textwrap.dedent('''
-    
-        This is a Python script that can be used to download YouTube videos or playlists. The script uses the `yt_dlp` library, which is a fork of `youtube-dl`. It can handle different types of YouTube URLs and allows the user to choose between downloading a video or a playlist.
 
-        Usage:
-            python youtube_downloader.py <url> -d=<type>
+Usage:
+======
+	python youtube_downloader.py <url> -d=<type>
 
-        Arguments:
-            <url>                   The URL of the YouTube video or playlist.
-        
-        Options:
-            -d=<type>, --download=<type>
-                                    Specify whether to download a video or playlist. Default is 'video'.
-            -h, --help              Show this help message and exit.
+Examples:
+=========
+To download a video:
+	python youtube_downloader.py "https://www.youtube.com/watch?v=5PS2p1AZzFY" -d=vide0
 
-        Examples:
-            To download a video:
-                python youtube_downloader.py "https://www.youtube.com/watch?v=5PS2p1AZzFY" -d=video
+To download a playlist:
+	python youtube_downloader.py "https://www.youtube.com/playlist?list=PLXmi76euGSyyq1nw21U1M4tTsM0Zysayk" -d=playlist
 
-            To download a playlist:
-                python youtube_downloader.py "https://www.youtube.com/playlist?list=PLXmi76euGSyyq1nw21U1M4tTsM0Zysayk" -d=playlist
-    
-    ''')
+Have fun!
+''')
 
     # Parse the URL and the download type from the command line arguments
-    parser = argparse.ArgumentParser(description="Download a YouTube video or playlist.", add_help=False, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(description="Download a YouTube video or playlist.", formatter_class=argparse.RawDescriptionHelpFormatter, add_help=help_message, epilog=help_message)
     parser.add_argument("url", help="The URL of the YouTube video or playlist.")
     parser.add_argument("-d", "--download", metavar="type", help="Specify whether to download a video or playlist. Default is 'video'.", choices=["video", "playlist"], default="video")
-    parser.add_argument("-h", "--help", action="help", default=argparse.SUPPRESS, help=argparse.SUPPRESS)
+    # parser.add_argument("-h", "--help", action="help", help=help_message)
     args = parser.parse_args()
 
     # Parse the video or playlist ID from the URL
